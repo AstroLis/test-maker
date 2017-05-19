dc3c104e9d0da0dde9f1d48c4b707b805ec89981
Варвара Танюхина < >
2017-05-19 22:53:02 +0300

    моноиды

--- algebra_tm.py
+++ algebra_tm.py
@@ -3,6 +3,15 @@ import random
 def test_union(a):
     return sum([(i|j not in a) for i in a for j in a])
 
+def test_union_monoid(a):
+    return sum([sum([(i|j == i) for i in a])==len(a) for j in a])
+
+def test_intersection_monoid(a):
+    return sum([sum([(i&j == i) for i in a])==len(a) for j in a])
+
+def test_zazh_monoid(a):
+    return sum([sum([(i^j == i) for i in a])==len(a) for j in a])
+    
 def test_intersection(a):
     return sum([(i&j not in a) for i in a for j in a])
 
@@ -11,7 +20,7 @@ def test_zazhig(a):
 
 def test_dif(a):
     return sum([(i-j not in a) for i in a for j in a])
-def MakeAlgebraTM(aFl=[1,1,1,1]):
+def MakeAlgebraTM(aFl=[1,1,1,1],i_mon=1):
  nt=10000
  for i in range(0,nt):
   A0=[random.randint(1,4) for i in range(1,10)]
@@ -23,32 +32,52 @@ def MakeAlgebraTM(aFl=[1,1,1,1]):
   #print(ta1)
   #print(aFl)
   if(aFl == ta1):
+      if(aFl==[0,1,0,0]):
+       if(test_intersection_monoid(A)!=i_mon):
+        continue
       return [list(j) for j in A]
+      #return A
+
+#for i in range(0,10):
+#  a=MakeAlgebraTM([0,1,0,0],0)         
+#  ii=test_union_monoid(a)
+#  ii=test_intersection_monoid(a)
+#  ii=test_zazh_monoid(a)
+#  if(ii==0):
+#   print([0,0,1,0],[list(j) for j in a])
+#   print(ii)
+#print([0,0,0,1],MakeAlgebraTM([0,0,0,1]))
+#print([0,0,1,0],MakeAlgebraTM([0,0,1,0]))#
+#print([0,0,1,1],MakeAlgebraTM([0,0,1,1]))
+#print([0,1,0,0],MakeAlgebraTM([0,1,0,0]))#
+#print([0,1,0,1],MakeAlgebraTM([0,1,0,1]))#
+#print([0,1,1,0],MakeAlgebraTM([0,1,1,0]))#
+#print([0,1,1,1],MakeAlgebraTM([0,1,1,1]))
+#print([1,0,0,0],MakeAlgebraTM([1,0,0,0]))
+#print([1,0,0,1],MakeAlgebraTM([1,0,0,1]))
+#print([1,0,1,0],MakeAlgebraTM([1,0,1,0]))
+#print([1,0,1,1],MakeAlgebraTM([1,0,1,1]))
+#print([1,1,0,0],MakeAlgebraTM([1,1,0,0]))
+#print([1,1,0,1],MakeAlgebraTM([1,1,0,1]))
+#print([1,1,1,0],MakeAlgebraTM([1,1,1,0]))
+#print([1,1,1,1],MakeAlgebraTM([1,1,1,1]))
+##print(MakeAlgebra())
+##
+##
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
-      
-def MakeAlgebraTM0():
-         A0=[random.randint(2,4) for i in range(1,10)]
-         A1=set([frozenset([random.randint(0,7) for i in range(0,A0[j])]) for j in range(0,4)])
-        # B1=set([frozenset(sum([list(j) for j in A1],[]))])
-        # B2=set([frozenset(frozenset.intersection(*A1))])
-        # B3=A1|B1|B2
-         B3=set([i.intersection(j) for i in A1 for j in A1])
-         print(A1)
-         print(B3)
-         E=[list(j) for j in B3]
-         return E
-#print(MakeAlgebraTM([0,0,0,1]))
-#print(MakeAlgebraTM([0,0,1,0]))#
-#print(MakeAlgebraTM([0,0,1,1]))
-#print(MakeAlgebraTM([0,1,0,0]))#
-#print(MakeAlgebraTM([0,1,0,1]))#
-#print(MakeAlgebraTM([0,1,1,1]))
-#print(MakeAlgebraTM([1,0,0,1]))
-#print(MakeAlgebraTM([1,0,1,0]))
-#print(MakeAlgebraTM([1,0,1,1]))
-#print(MakeAlgebraTM([1,1,0,0]))
-#print(MakeAlgebraTM([1,1,0,1]))
-#print(MakeAlgebraTM([1,1,1,0]))
-#print(MakeAlgebraTM([1,1,1,1]))
-#print(MakeAlgebra())
